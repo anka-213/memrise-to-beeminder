eed24586b6b0408684eeb0a14038f78b1a237601
Andreas Källberg <anka.213@gmail.com>
2015-09-18 11:26:09 +0200

    Use Memrise (internal) API instead
    Also using requests instead of mechanize

--- memrise.rb
+++ memrise.rb
@@ -1,42 +1,18 @@
 #!/usr/bin/env ruby
 
-require 'mechanize'
+require "requests/sugar"
 
 class Memrise
-
-  LOGIN_URL = 'https://www.memrise.com/login/'
-
   public
   def initialize args = {}
     raise 'Username not provided.' unless args[:username]
-    raise 'Password not provided.' unless args[:password]
-
-    @agent = args[:agent] || Mechanize.new
 
     @username = args[:username]
-    @password = args[:password]
-
-    login
   end
 
   def points
-    raise 'Dashboard (Home) not loaded.' unless @dashboard
-    @dashboard
-        .search('.content-stats .right .number').first.content
-        .gsub(',', '').to_i
-  end
-
-  private
-  def login
-    login_page = @agent.get LOGIN_URL
-    login_form = login_page.forms.first
-
-    login_form.username = @username
-    login_form.password = @password
-
-    dashboard = @agent.submit login_form
-    raise 'Login failed.' if dashboard.search('body.dashboard').empty?
-    @dashboard = dashboard
+    r=Requests.get("http://www.memrise.com/api/user/get/?user_username=#{@username}&with_leaderboard=true")
+    r.json()['user']['leaderboard']['points_alltime']
   end
 
 end
